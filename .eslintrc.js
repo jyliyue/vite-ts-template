5dc3d97081c9556ad58f973494acb97d316caf3a
jiangliyue <772748611@qq.com>
2022-02-25 13:32:03 +0800

    chore: 处理prettier与eslint兼容

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,12 @@ module.exports = {
         es2021: true,
         node: true
     },
-    extends: ['plugin:vue/vue3-recommended', 'standard'],
+    extends: [
+        'plugin:vue/vue3-recommended',
+        'standard',
+        // 新增，必须放在最后面
+        'plugin:prettier/recommended'
+    ],
     parserOptions: {
         ecmaVersion: 'latest',
         parser: '@typescript-eslint/parser',
--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img
-    alt="Vue logo"
-    src="./assets/logo.png"
-  >
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <style>
