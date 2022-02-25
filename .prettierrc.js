d39250b159eaf88b91ce2966586430aa9057d03d
jiangliyue <772748611@qq.com>
2022-02-25 11:05:26 +0800

    chore: 配置prettier

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    printWidth: 200, //单行长度
+    tabWidth: 4, //缩进长度
+    semi: false, //句末使用分号
+    singleQuote: true, //使用单引号
+    trailingComma: 'none' // 句末逗号
+}
--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
