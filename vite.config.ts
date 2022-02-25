fae6c75a6041ef8a8bf8d79a0425912c0c78eca4
jiangliyue <772748611@qq.com>
2022-02-25 13:50:13 +0800

    chore: 优化检测脚本命令

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    eslintPlugin({
-      // 配置
-      cache: false // 禁用 eslint 缓存
-    })
-  ]
+    plugins: [
+        vue(),
+        eslintPlugin({
+            // 配置
+            cache: false // 禁用 eslint 缓存
+        })
+    ]
 })
