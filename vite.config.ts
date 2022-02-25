773d1ef6b01048c743905df5caea56053ad7cddd
jiangliyue <772748611@qq.com>
2022-02-25 10:07:50 +0800

    chore: 配置运行时校验控制台输出错误

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    eslintPlugin({
+      // 配置
+      cache: false // 禁用 eslint 缓存
+    })
+  ]
 })
