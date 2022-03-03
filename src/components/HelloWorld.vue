7ed117eff6c2d87e3ca5154014eb8304f339433a
jiangliyue <772748611@qq.com>
2022-03-03 15:14:21 +0800

    chore: add ElementPlus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@ const count = ref(0)
 
 <template>
     <h1>{{ msg }}</h1>
-
+    <el-button>Default</el-button>
     <p>
         Recommended IDE setup:
         <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +12,12 @@ export default defineConfig({
         eslintPlugin({
             // 配置
             cache: false // 禁用 eslint 缓存
+        }),
+        AutoImport({
+            resolvers: [ElementPlusResolver()]
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()]
         })
     ]
 })
