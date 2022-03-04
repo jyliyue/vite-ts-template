0acf9d942265f156196bbf6cde6c29bbca238b96
jiangliyue <772748611@qq.com>
2022-03-04 17:23:48 +0800

    feat: ajax

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import Debounced from '../test/debounce'
 import Throttle from '../test/throttle'
+import Ajax from '../test/ajax'
 defineProps<{ msg: string }>()
 
 const count = ref(0)
@@ -11,12 +12,20 @@ const debounce = Debounced.use(function (a: number) {
 const throttle = Throttle.use(function (a: number) {
     console.log(a)
 })
+
+const url = 'https://www.fastmock.site/mock/871b3e736e653b99374b7713e4011f9f/boss/user/list'
+const get = function () {
+    Ajax.get(url, (response: string) => {
+        console.log(response)
+    })
+}
 </script>
 
 <template>
     <h1>{{ msg }}</h1>
     <el-button @click="debounce(1)">debounce</el-button>
     <el-button @click="throttle(2)">throttle</el-button>
+    <el-button @click="get">get</el-button>
     <p>
         Recommended IDE setup:
         <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
